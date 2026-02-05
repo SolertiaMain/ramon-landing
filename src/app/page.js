3a7297982ff402c3a06b11960e94e6bdfe9a2945
Misthyc32 <edfrancowork@hotmail.com>
2026-02-05 16:37:29 -0600

    correccion de sections

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,16 @@
 import Hero from "../components/sections/Hero";
 import Statement from "../components/sections/Statement";
 import Letter from "../components/sections/Letter";
-import Certifications from "../components/sections/Certifications";
-import Workshops from "../components/sections/Workshops";
-import Podcast from "../components/sections/Podcast";
-import Blog from "../components/sections/Blog";
 import Contact from "../components/sections/Contact";
 import { site } from "../content/site";
 
 export default function Page() {
   return (
-    <>
-
-      <main>
-        <Hero data={site.hero} />
-        <Statement data={site.statement} />
-        <Letter data={site.letter} />
-
-        <Certifications data={site.certifications} />
-        <Workshops data={site.workshops} />
-        <Podcast data={site.podcast} />
-        <Blog data={site.blog} />
-
-        <Contact data={site.contact} />
-      </main>
-
-    </>
+    <main>
+      <Hero data={site.hero} />
+      <Statement data={site.statement} />
+      <Letter data={site.letter} />
+      <Contact data={site.contact} />
+    </main>
   );
 }
